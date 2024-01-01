48a47093fe0df428c37896530a80839a5ad2d5a3
GuRaBang <qkrdbsgk00@gmail.com>
2024-01-02 01:15:36 +0900

    feat: 주문현황 검색 기능 추가

--- lib/features/order_status/presentation/pages/order_status.page.dart
+++ lib/features/order_status/presentation/pages/order_status.page.dart
@@ -3,8 +3,39 @@ import 'package:check_order_admin/features/order_status/presentation/widgets/dat
 import 'package:check_order_admin/features/order_status/presentation/widgets/order_status_table.dart';
 import 'package:check_order_admin/features/order_status_management/data/models/order_model.dart';
 import 'package:check_order_admin/features/order_status_management/data/models/menu_model.dart';
+import 'package:intl/intl.dart';
 
-List<OrderModel> orderModels = [
+List<OrderModel> filterOrders(
+  List<OrderModel> orders,
+  DateTime? startDate,
+  DateTime? endDate,
+  String tableName,
+) {
+  List<OrderModel> filteredOrders = orders.where((order) {
+    final orderedAtDate = DateTime(
+      order.orderedAt.year,
+      order.orderedAt.month,
+      order.orderedAt.day,
+    );
+
+    final containsTableName = order.tableId.contains(tableName);
+
+    if (startDate == null || endDate == null) {
+      return containsTableName;
+    }
+
+    final isDateInRange = (orderedAtDate.isAfter(startDate) ||
+            orderedAtDate.isAtSameMomentAs(startDate)) &&
+        (orderedAtDate.isBefore(endDate) ||
+            orderedAtDate.isAtSameMomentAs(endDate));
+
+    return isDateInRange && containsTableName;
+  }).toList();
+
+  return filteredOrders;
+}
+
+List<OrderModel> orders = [
   OrderModel(
     id: 'id',
     tableId: 'A1',
@@ -18,20 +49,40 @@ List<OrderModel> orderModels = [
     orderedAt: DateTime.now(),
   ),
   OrderModel(
-      id: 'id',
-      tableId: 'B1',
-      storeId: 'storeId',
-      status: OrderStatus.accepted,
-      orderType: '메뉴',
-      menus: const [
-        MenuModel(id: '1', name: '육개장', totalPrice: 3000, count: 2),
-        MenuModel(id: '1', name: '계란말이', totalPrice: 50000, count: 2),
-      ],
-      orderedAt: DateTime.now())
+    id: 'id',
+    tableId: 'B1',
+    storeId: 'storeId',
+    status: OrderStatus.accepted,
+    orderType: '메뉴',
+    menus: const [
+      MenuModel(id: '1', name: '육개장', totalPrice: 3000, count: 2),
+      MenuModel(id: '1', name: '계란말이', totalPrice: 50000, count: 2),
+    ],
+    orderedAt: DateTime.now(),
+  ),
+  OrderModel(
+    id: 'id',
+    tableId: 'A2',
+    storeId: 'storeId',
+    status: OrderStatus.accepted,
+    orderType: '메뉴',
+    menus: const [
+      MenuModel(id: '1', name: '육개장', totalPrice: 3000, count: 2),
+      MenuModel(id: '1', name: '계란말이', totalPrice: 50000, count: 2),
+    ],
+    orderedAt: DateTime(2024, 01, 3),
+  ),
 ];
 
-class OrderStatusPage extends StatelessWidget {
+class OrderStatusPage extends StatefulWidget {
   const OrderStatusPage({super.key});
+
+  @override
+  State<OrderStatusPage> createState() => _OrderStatusPageState();
+}
+
+class _OrderStatusPageState extends State<OrderStatusPage> {
+  List<OrderModel> filteredOrders = orders;
 
   @override
   Widget build(BuildContext context) {
@@ -40,10 +91,22 @@ class OrderStatusPage extends StatelessWidget {
         children: [
           DateRangeSearchInput(
             onConfirm: (v) {
-              print('${v.startDate} ${v.endDate} ${v.tableName} 검색결과');
+              DateTime? startDate =
+                  DateFormat('yyyy-MM-dd').tryParse(v.startDate);
+              DateTime? endDate = DateFormat('yyyy-MM-dd').tryParse(v.endDate);
+
+              setState(() {
+                filteredOrders =
+                    filterOrders(orders, startDate, endDate, v.tableName ?? '');
+              });
+            },
+            onReset: () {
+              setState(() {
+                filteredOrders = orders;
+              });
             },
           ),
-          Expanded(child: OrderStatusTable(orders: orderModels)),
+          Expanded(child: OrderStatusTable(orders: filteredOrders)),
         ],
       ),
     );
