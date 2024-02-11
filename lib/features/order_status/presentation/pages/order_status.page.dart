701e930ec4fdbd93bedc28200a4d72acfb6d9c13
GuRaBang <qkrdbsgk00@gmail.com>
2024-02-11 18:59:19 +0900

    feat: 주문 기록 페이지 백엔드 연동

--- lib/features/order_status/presentation/pages/order_status.page.dart
+++ lib/features/order_status/presentation/pages/order_status.page.dart
@@ -1,10 +1,10 @@
-import 'package:check_order_admin/features/menu_management/data/models/menu_item.dart';
 import 'package:check_order_admin/features/order_status/presentation/widgets/date_range_search_input.dart';
 import 'package:check_order_admin/features/order_status/presentation/widgets/order_status_table.dart';
-import 'package:check_order_admin/features/order_status_management/data/models/order_item.dart';
 import 'package:check_order_admin/features/order_status_management/data/models/order_model.dart';
+import 'package:check_order_admin/services/auth_provider.dart';
+import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 DateTime removeTime(DateTime date) {
   return DateTime(date.year, date.month, date.day);
@@ -20,7 +20,6 @@ List<OrderModel> filterOrders({
     final modifiedOrderedAtDate = removeTime(order.orderedAt);
     final modifiedStartDate = removeTime(startDate);
     final modifiedEndDate = removeTime(endDate);
-
     final containsTableName = order.tableName.contains(tableName);
 
     final isDateInRange = (modifiedOrderedAtDate.isAfter(modifiedStartDate) ||
@@ -34,91 +33,17 @@ List<OrderModel> filterOrders({
   return filteredOrders;
 }
 
-List<OrderModel> orders = [
-  OrderModel(
-    id: 'id',
-    tableName: 'A1',
-    storeId: 'storeId',
-    status: OrderStatus.accepted,
-    orderType: '메뉴',
-    items: [
-      OrderItemModel(
-          id: '1',
-          item: MenuItemModel(id: '1', name: '육개장', category: '탕'),
-          updatedAt: DateTime.now(),
-          totalAmount: 3000),
-      OrderItemModel(
-          id: '2',
-          item: MenuItemModel(id: '2', name: '계란말이', category: '탕'),
-          updatedAt: DateTime.now(),
-          totalAmount: 50000),
-    ],
-    orderedAt: DateTime.now(),
-    totalAmount: 53000,
-  ),
-  OrderModel(
-    id: 'id',
-    tableName: 'B1',
-    storeId: 'storeId',
-    status: OrderStatus.accepted,
-    orderType: '메뉴',
-    items: [
-      OrderItemModel(
-          id: '1',
-          item: MenuItemModel(id: '1', name: '육개장', category: '탕'),
-          updatedAt: DateTime.now(),
-          totalAmount: 3000),
-      OrderItemModel(
-          id: '2',
-          item: MenuItemModel(id: '2', name: '계란말이', category: '탕'),
-          updatedAt: DateTime.now(),
-          totalAmount: 50000),
-    ],
-    orderedAt: DateTime.now(),
-    totalAmount: 53000,
-  ),
-  OrderModel(
-    id: 'id',
-    tableName: 'A2',
-    storeId: 'storeId',
-    status: OrderStatus.accepted,
-    orderType: '메뉴',
-    items: [
-      OrderItemModel(
-          id: '1',
-          item: MenuItemModel(id: '1', name: '육개장', category: '탕'),
-          updatedAt: DateTime.now(),
-          totalAmount: 3000),
-      OrderItemModel(
-          id: '2',
-          item: MenuItemModel(id: '2', name: '계란말이', category: '탕'),
-          updatedAt: DateTime.now(),
-          totalAmount: 50000),
-    ],
-    orderedAt: DateTime(2024, 01, 3),
-    totalAmount: 53000,
-  ),
-];
-
-class OrderStatusPage extends StatefulWidget {
+class OrderStatusPage extends ConsumerStatefulWidget {
   const OrderStatusPage({super.key});
 
   @override
-  State<OrderStatusPage> createState() => _OrderStatusPageState();
+  _OrderStatusPageState createState() => _OrderStatusPageState();
 }
 
-class _OrderStatusPageState extends State<OrderStatusPage> {
+class _OrderStatusPageState extends ConsumerState<OrderStatusPage> {
   DateTime startDate = DateTime.now();
   DateTime endDate = DateTime.now();
   String tableName = '';
-  late List<OrderModel> filteredOrders;
-
-  @override
-  void initState() {
-    super.initState();
-    filteredOrders =
-        filterOrders(orders: orders, startDate: startDate, endDate: endDate);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -134,12 +59,6 @@ class _OrderStatusPageState extends State<OrderStatusPage> {
                 startDate = v.startDate;
                 endDate = v.endDate;
                 tableName = v.tableName ?? '';
-                filteredOrders = filterOrders(
-                  orders: orders,
-                  startDate: startDate,
-                  endDate: endDate,
-                  tableName: tableName,
-                );
               });
             },
             onReset: () {
@@ -147,18 +66,33 @@ class _OrderStatusPageState extends State<OrderStatusPage> {
                 startDate = DateTime.now();
                 endDate = DateTime.now();
                 tableName = '';
-
-                filteredOrders = filterOrders(
-                  orders: orders,
-                  startDate: startDate,
-                  endDate: endDate,
-                );
               });
             },
           ),
-          Expanded(child: OrderStatusTable(orders: filteredOrders)),
+          Expanded(child: _buildOrderStatusTable(ref)),
         ],
       ),
+    );
+  }
+
+  Widget _buildOrderStatusTable(WidgetRef ref) {
+    return FirestoreBuilder(
+      ref: ordersRef,
+      builder: (_, AsyncSnapshot<OrderModelQuerySnapshot> snapshot, __) {
+        final orders = (snapshot.data?.docs.map((e) => e.data).where((data) =>
+                    data.storeId ==
+                    ref.read(authProvider.notifier).currentUser?.email) ??
+                [])
+            .toList();
+
+        final filteredOrders = filterOrders(
+            orders: orders,
+            startDate: startDate,
+            endDate: endDate,
+            tableName: tableName);
+
+        return OrderStatusTable(orders: filteredOrders);
+      },
     );
   }
 }
